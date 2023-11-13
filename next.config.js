85587bb308c5536a56ff033f1c970863cc128f62
Zhigeng Qian <timeseven@192-168-1-3.tpgi.com.au>
2023-11-13 17:20:44 +1100

    fix: next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "https://ecommerce-store-black-mu.vercel.app/api/:path*",
+        source: "/api/:path*",
         // destination: "http://localhost:3000/api/:path*",
         destination: "https://ecommerce-admin-eosin-chi.vercel.app/api/:path*",
       },

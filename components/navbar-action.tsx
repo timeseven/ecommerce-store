487c482757ef207a7949b818299d0c174660b036
Zhigeng Qian <timeseven@192-168-1-3.tpgi.com.au>
2023-11-16 16:57:22 +1100

    fix: bugs

--- components/navbar-action.tsx
+++ components/navbar-action.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 import { ShoppingBag } from "lucide-react";
 
-import Button from "@/components/ui/button";
 import useCart from "@/hooks/use-cart";
 import Link from "next/link";
 
@@ -16,7 +14,6 @@ const NavbarActions = () => {
   }, []);
 
   const cart = useCart();
-  const router = useRouter();
 
   if (!isMounted) {
     return null;
@@ -25,10 +22,10 @@ const NavbarActions = () => {
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Link href={`/order`}>My Orders</Link>
-      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
+      <Link href={"/cart"} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color="white" />
         <span className="ml-2 text-sm font-medium text-white">{cart.items.length}</span>
-      </Button>
+      </Link>
     </div>
   );
 };
